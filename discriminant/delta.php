597511d22c0051728668a6cbbdb071afc19eaa2b
Miarina Andrianantoanina <104420478+andrianantoanina@users.noreply.github.com>
2022-12-20 04:13:22 -0800

    Update delta.php

--- discriminant/delta.php
+++ discriminant/delta.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
 <head>
 	<title>Résolution de polynômes du second degré (ax² + bx + c)</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" type="text/css" media="screen" />
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 </head>
 <body>
